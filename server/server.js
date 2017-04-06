4e933095de50ae48f2f6e58ecc3cc71b062ed861
Robert <seedraev@gmail.com>
2017-04-06 16:59:54 +0000

    initial commit

---
+++ server/server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import path from 'path';
+
+
+let app = express();
+
+app.get('/' , (req,res)=>{
+    res.sendFile(path.join(__dirname,'./index.html'));
+} );
+
+
+app.listen(process.env.PORT, ()=>{
+    console.log('Server starts');
+    
+});
